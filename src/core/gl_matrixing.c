3f40b95679e539174b271973974b1850510b9591
unknown <aemilien@student.42.fr>
2019-05-09 19:46:21 +0200

    ++

--- src/core/gl_matrixing.c
+++ src/core/gl_matrixing.c
@@ -20,10 +20,10 @@ void gl_matrixing(t_software_environ *env)
 	env->model_matrix = identity_mat4();
 
 	env->projection_matrix = perspective_mat4(
-		deg_to_rad(60.0f),
+		60.0f,
 		800.0f / 600.0f,
-		0.0f,
-		1000.0f);
+		1.0f,
+		200.0f);
 
 	env->mvp_uni = glGetUniformLocation(env->program_id, "mvp");
 
--- src/libmatrix/src/mat4/perspective_mat4.c
+++ src/libmatrix/src/mat4/perspective_mat4.c
@@ -1,5 +1,5 @@
 #include "libmatrix.h"
-
+/*
 t_mat4 *perspective_mat4(
 	GLfloat fov,
 	GLfloat aspect,
@@ -22,4 +22,35 @@ t_mat4 *perspective_mat4(
 	/ (far_plane - near_plane) * -1;
 
 	return (ret);
+}
+*/
+
+/*
+** Creates a projection matrix like gluPerspective or glFrustum.
+** Upload to your shader as usual.
+*/
+
+t_mat4 *perspective_mat4(
+	GLfloat fov,
+	GLfloat aspect,
+    GLfloat znear,
+	GLfloat zfar)
+{
+		GLfloat top;
+		GLfloat right;
+
+		t_mat4 *ret;
+		ret = identity_mat4();
+
+		top = tanf(deg_to_rad(fov) / 2.0f) * znear;
+		right = top * aspect;
+
+		ret->value[0] = znear / right;
+		ret->value[5] = znear / top;
+		ret->value[10] = -(zfar + znear) / (zfar - znear);
+		ret->value[11] = -2.0f * zfar * znear / (zfar - znear);
+		ret->value[14] = -1.0f;
+
+    return ret;
+
 }
