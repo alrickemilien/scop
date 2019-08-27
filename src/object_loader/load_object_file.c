71b2e1020c9058f5a10496530f14a5f0d1308be7
Alrick EMILIEN <aemilien@e1r10p16.42.fr>
2019-08-27 21:50:03 +0200

    Fix object loaderr garbage

--- src/object_loader/load_object_file.c
+++ src/object_loader/load_object_file.c
@@ -98,7 +98,7 @@ int			load_object_file(t_mesh *data, const char *file_path)
 	data->path = strdup(file_path);
 	if (!(fp = fopen(file_path, "r")))
 		return (-1);
-	while (fgets(line, BUFF_SIZE, fp) != NULL && ++g_current_line)
+	while (fgets(line, LOADER_LINE_BUFF_SIZE, fp) != NULL && ++g_current_line)
 	{
 		if (line[0] == '\n')
 			continue ;
